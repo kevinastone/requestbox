c17f99a224f35c0e6da26164e6ddbabab8526ed1
Kevin Stone <kevinastone@gmail.com>
2015-11-06 12:32:39 -0800

    Added Timex support

--- web/models/request.ex
+++ web/models/request.ex
@@ -1,76 +1,68 @@
-defmodule Phoenixbin do
+defmodule Phoenixbin.Header do
+  defstruct [:name, :value]
 
-  defmodule Header do
-    defstruct [:name, :value]
+  defmodule Type do
 
-    defmodule Type do
+    @behaviour Ecto.Type
+    def type, do: :text
 
-      @behaviour Ecto.Type
-      def type, do: :text
+    def cast(%Phoenixbin.Header{} = header), do: header
+    def cast(%{} = header), do: struct(Phoenixbin.Header, header)
 
-      def cast(%Header{} = header), do: header
-      def cast(%{} = header), do: struct(Header, header)
+    def load(value), do: Poison.decode(value, as: Phoenixbin.Header)
+    def dump(value), do: Poison.encode(value)
+  end
+end
 
-      def load(value), do: Poison.decode(value, as: Phoenixbin.Header)
-      def dump(value), do: Poison.encode(value)
+defmodule Phoenixbin.Headers do
+  defmodule Type do
+
+    @behaviour Ecto.Type
+    def type, do: :text
+
+    def cast(headers) when is_list(headers) do
+      {:ok, Enum.map(headers, &Phoenixbin.Header.Type.cast/1)}
     end
+    def cast(_other), do: :error
+
+    def load(value), do: Poison.decode(value, as: [Phoenixbin.Header])
+    def dump(value), do: Poison.encode(value)
+  end
+end
+
+defmodule Phoenixbin.Request do
+  use Phoenixbin.Web, :model
+  use Timex.Ecto.Timestamps
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "requests" do
+    field :method, :string
+    field :client_ip, :string
+    field :path, :string
+    field :query_string, :string
+    field :headers, Phoenixbin.Headers.Type
+    field :body, :string
+
+    belongs_to :session, Phoenixbin.Session
+    timestamps
   end
 
-  defmodule Headers do
-    defmodule Type do
+  @required_fields ~w(session_id method path)
+  @optional_fields ~w(headers body query_string)
 
-      @behaviour Ecto.Type
-      def type, do: :text
+  @doc """
+  Creates a changeset based on the `model` and `params`.
 
-      def cast(headers) when is_list(headers) do
-        {:ok, Enum.map(headers, &Header.Type.cast/1)}
-      end
-      def cast(_other), do: :error
-
-      def load(value), do: Poison.decode(value, as: [Header])
-      def dump(value), do: Poison.encode(value)
-    end
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
   end
 
-  defmodule Request do
-    use Phoenixbin.Web, :model
-    # use Timex.Ecto.Timestamps
-
-    @primary_key {:id, :binary_id, autogenerate: true}
-    schema "requests" do
-      field :method, :string
-      field :client_ip, :string
-      field :path, :string
-      field :query_string, :string
-      field :headers, Phoenixbin.Headers.Type
-      field :body, :string
-
-      belongs_to :session, Phoenixbin.Session
-      timestamps
-    end
-
-    @required_fields ~w(session_id method path)
-    @optional_fields ~w(headers body query_string)
-
-    @doc """
-    Creates a changeset based on the `model` and `params`.
-
-    If no params are provided, an invalid changeset is returned
-    with no validation performed.
-    """
-    def changeset(model, params \\ :empty) do
-      model
-      |> cast(params, @required_fields, @optional_fields)
-    end
-
-    def sorted(query) do
-      from p in query,
-      order_by: [desc: p.inserted_at]
-    end
-
-  def format_date(date) do
-    date
-    |> Timex.DateFormat.format!("%B %e, %Y", :strftime)
-  end
+  def sorted(query) do
+    from p in query,
+    order_by: [desc: p.inserted_at]
   end
 end
