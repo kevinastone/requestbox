3994ec419c728478cb97620568fa7edab2266c03
Kevin Stone <kevinastone@gmail.com>
2016-01-21 10:48:36 -0800

    Added env var for the hostname for url generation

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :requestbox, Requestbox.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "enigmatic-stream-8949.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
