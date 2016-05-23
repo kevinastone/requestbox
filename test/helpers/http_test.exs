cacac4fa9182e73b83c79cad7920a947769a4756
Kevin Stone <kevinastone@gmail.com>
2016-05-22 17:06:13 -0700

    Added prismjs syntax highlighting

---
+++ test/helpers/http_test.exs
@@ -0,0 +1,16 @@
+defmodule Requestbox.Helpers.HTTPTest do
+  use ExUnit.Case, async: true
+  use Requestbox.ConnCase
+
+  alias Requestbox.Helpers.HTTP
+
+  test "Provided Content Type" do
+    headers = [%Requestbox.Request.Header{name: "content-type", value: "application/random"}]
+    assert HTTP.language_content_type(headers) == "random"
+  end
+
+  test "Missing Content Type" do
+    headers = [%Requestbox.Request.Header{name: "host", value: "example.com"}]
+    assert HTTP.language_content_type(headers, "blah") == "blah"
+  end
+end
