5a198ca05b90f6a640c99934a492dbc120023bbf
Kevin Stone <kevinastone@gmail.com>
2016-05-23 07:17:30 -0700

    Added pretty-printer for JSON

--- web/static/js/highlight.js
+++ web/static/js/highlight.js
@@ -3,4 +3,21 @@ import Prism from 'prismjs';
 import _PrismHTTP from 'prismjs/components/prism-http';
 import _PrismJSON from 'prismjs/components/prism-json';
 
-document.addEventListener('DOMContentLoaded', () => Prism.highlightAll());
+function selector(lang) {
+  return `code.language-${lang}, .language-${lang} > code, code.lang-${lang}, .lang-${lang} > code`;
+}
+
+function prettyJSON() {
+  for (const element of Array.from(document.querySelectorAll(selector('json')))) {
+    try {
+      element.innerText = JSON.stringify(JSON.parse(element.innerText), null, 2);
+    } catch (error) {
+      // do nothing
+    }
+  }
+}
+
+export default () => {
+  prettyJSON();
+  Prism.highlightAll();
+};
