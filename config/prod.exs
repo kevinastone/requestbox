14d66745468f0e25d69c5efdeebb2d44b7a3a748
Kevin Stone <kevinastone@gmail.com>
2017-07-28 18:17:46 -0700

    Bumped to Phoenix 1.3

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :requestbox, Requestbox.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Requestbox.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.4"},
+    [{:phoenix, "~> 1.3.0"},
      {:phoenix_html, "~> 2.6"},
      {:ecto, "~> 2.1"},
      {:phoenix_ecto, "~> 3.0"},
