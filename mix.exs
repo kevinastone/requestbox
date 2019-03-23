11959751b742f9ae9d623dcd9122db877286c246
Kevin Stone <kevinastone@gmail.com>
2019-03-23 09:48:47 -0500

    Bumped minimum elixir to 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Requestbox.Mixfile do
     [
       app: :requestbox,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: false],
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
