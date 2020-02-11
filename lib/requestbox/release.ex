53a2501950375ab5873917899683a7c37f0c9d96
Kevin Stone <kevinastone@gmail.com>
2020-02-11 13:13:16 -0600

    Added create release helper

--- lib/requestbox/release.ex
+++ lib/requestbox/release.ex
@@ -1,7 +1,23 @@
 defmodule Requestbox.Release do
   @app :requestbox
 
+  def create do
+    for repo <- repos() do
+      case repo.__adapter__.storage_up(repo.config) do
+        :ok ->
+          IO.puts "The database for #{inspect repo} has been created"
+        {:error, :already_up} ->
+          IO.puts "The database for #{inspect repo} has already been created"
+        {:error, term} when is_binary(term) ->
+          IO.puts "The database for #{inspect repo} couldn't be created: #{term}"
+        {:error, term} ->
+          IO.puts "The database for #{inspect repo} couldn't be created: #{inspect term}"
+      end
+    end
+  end
+
   def migrate do
+    Application.ensure_all_started(:ecto)
     for repo <- repos() do
       # Ecto 3+
       # {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
